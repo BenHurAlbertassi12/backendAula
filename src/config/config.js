ace4dc340c16394f0909cfad278bdbd1105e314c
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-01-23 15:50:50 -0300

    pt 1 course

--- src/config/config.js
+++ src/config/config.js
@@ -1,27 +1,26 @@
-require('dotenv').config()
+// src/config/config.js
 
-const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT } = process.env
+require('dotenv').config();
+
+const config = {
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql',
+};
 
 module.exports = {
   development: {
-    username: DB_USER || "root",
-    password: DB_PASSWORD || "password",
-    database: DB_NAME || "database_development",
-    host: DB_HOST || "127.0.0.1",
-    dialect: DB_DIALECT || "mysql"
-},
+    ...config,
+    database: 'employees_associations_development',
+  },
   test: {
-    username: DB_USER || "root",
-    password: DB_PASSWORD || "password",
-    database: DB_NAME || "database_development",
-    host: DB_HOST || "127.0.0.1",
-    dialect: DB_DIALECT || "mysql"
-},
+    ...config,
+    database: 'employees_associations_test',
+    logging: false,
+  },
   production: {
-    username: DB_USER || "root",
-    password: DB_PASSWORD || "password",
-    database: DB_NAME || "database_development",
-    host: DB_HOST || "127.0.0.1",
-    dialect: DB_DIALECT || "mysql"
-}
-}
+    ...config,
+    database: 'employees_associations_production',
+  },
+};
